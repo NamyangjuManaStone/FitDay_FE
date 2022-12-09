9d6166c28defb2f4cdc71c3aa6606bba8afe6f5d
Jungjaeyeong <jjy8037@gmail.com>
2022-12-09 17:35:06 +0900

    feat: 유저 리덕스 추가

--- src/features/user/user.actions.js
+++ src/features/user/user.actions.js
@@ -1,7 +1,19 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { userAPIs } from '../../apis';
+import { tokenCookie } from '../../utils/cookie';
 
 export const signupAction = createAsyncThunk('user/signup', async (body) => {
-  const data = await userAPIs.signupAPI(body);
+  const { data } = await userAPIs.signupAPI(body);
+  return data;
+});
+
+export const loginAction = createAsyncThunk('user/login', async (body) => {
+  const { data } = await userAPIs.loginAPI(body);
+  tokenCookie.setCookie(data.accessToken);
+  return data;
+});
+
+export const checkMeAction = createAsyncThunk('user/checkMe', async () => {
+  const { data } = await userAPIs.checkMeAPI();
   return data;
 });
--- src/features/user/user.slice.js
+++ src/features/user/user.slice.js
@@ -2,17 +2,28 @@ import { createSlice } from '@reduxjs/toolkit';
 import * as actions from './user.actions';
 
 const initialState = {
-  myData: null,
+  user: null, // 내 정보
   signupLoading: false,
   signupDone: false,
   signupError: null,
+  loginLoading: false,
+  loginDone: false,
+  loginError: null,
+  checkMeLoading: false,
+  checkMeDone: false,
+  checkMeError: null,
 };
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {},
+  reducers: {
+    logoutAction: (state) => {
+      state.user = null;
+    },
+  },
   extraReducers: ({ addCase }) => {
+    // 회원가입
     addCase(actions.signupAction.pending, (state) => {
       state.signupLoading = true;
       state.signupDone = false;
@@ -26,7 +37,39 @@ export const userSlice = createSlice({
       state.signupLoading = false;
       state.signupError = action.error.message;
     });
+    // 로그인
+    addCase(actions.loginAction.pending, (state) => {
+      state.loginLoading = true;
+      state.loginDone = false;
+      state.loginError = null;
+    });
+    addCase(actions.loginAction.fulfilled, (state, action) => {
+      state.loginLoading = false;
+      state.loginDone = true;
+      state.user = action.payload.user;
+    });
+    addCase(actions.loginAction.rejected, (state, action) => {
+      state.loginLoading = false;
+      state.loginError = action.error.message;
+    });
+    // 내 정보 조회
+    addCase(actions.checkMeAction.pending, (state) => {
+      state.checkMeLoading = true;
+      state.checkMeDone = false;
+      state.checkMeError = null;
+    });
+    addCase(actions.checkMeAction.fulfilled, (state, action) => {
+      state.checkMeLoading = false;
+      state.checkMeDone = true;
+      state.user = action.payload.user;
+    });
+    addCase(actions.checkMeAction.rejected, (state, action) => {
+      state.checkMeLoading = false;
+      state.checkMeError = action.error.message;
+    });
   },
 });
+
+export const { logoutAction } = userSlice.actions;
 
 export default userSlice.reducer;
